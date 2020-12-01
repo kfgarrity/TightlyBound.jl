fe1c39d648d0f1302c5e625b8bb41c5397aedda7
kfgarrity <kevin.garrity@nist.gov>
2020-12-01 15:01:03 -0500

    quick update lappy

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,5 +2,10 @@ using TightlyBound
 using Test
 
 @testset "TightlyBound.jl" begin
+    TightlyBound.f()
+    TightlyBound.g()
+    TightlyBound.h()
+    TightlyBound.i()
+    TightlyBound.j()
     @test 3 == 2+1
 end
