3e5e8a73a5a9c95480d904569ad4e9b4f8fc0124
kfgarrity <kevin.garrity@nist.gov>
2020-12-08 19:16:58 -0500

    quick update lappy

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,15 +2,15 @@ using TightlyBound
 using Test
 
 include("crystal_testing.jl")
-#include("test_ewald.jl")
-#include("sym_test.jl")
-#include("test_laguerre.jl")
-#include("test_U.jl")
-#include("test_forces.jl")
-#include("test_examples.jl")
-#include("test_proto.jl")
-#include("test_makescf.jl")
-#include("test_atomicproj.jl")
+include("test_ewald.jl")
+include("sym_test.jl")
+include("test_laguerre.jl")
+include("test_U.jl")
+include("test_forces.jl")
+include("test_examples.jl")
+include("test_proto.jl")
+include("test_makescf.jl")
+include("test_atomicproj.jl")
 
 
 
