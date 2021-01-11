d7761548f8cae8e825cf217a9276af9099be613e
kfgarrity <kevin.garrity@nist.gov>
2021-01-10 21:52:09 -0500

    quick update lappy

--- src/SetDir.jl
+++ src/SetDir.jl
@@ -3,5 +3,5 @@ EXAMPLESDIR = joinpath(dirname(pathof(TightlyBound)), "..", "examples")
 TESTDIR = joinpath(dirname(pathof(TightlyBound)), "..", "test")
 TEMPLATEDIR = joinpath(dirname(pathof(TightlyBound)), "..", "template_inputs")
 STRUCTDIR = joinpath(dirname(pathof(TightlyBound)), "..", "reference_structures")
-DATSDIR = joinpath(dirname(pathof(TightlyBound)), "..", "dats", "pbesol", "v0.3")
+DATSDIR = joinpath(dirname(pathof(TightlyBound)), "..", "dats", "pbesol", "v0.4")
 
